7c10c0034df87635493f2389e73642c0042d3f01
Yevhenii Melnyk <yevheniimelnyk7@gmail.com>
2023-11-10 14:10:40 +0200

    new page contact

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-jQuery(document).ready(function ($) {
+// alert('Hello world!');
 
-});
+// let age = prompt("promt");
+// console.log(age);
+
+let yourAge = confirm("Вам есть 18 лет?");
+var content = yourAge ? "18+" : "Нет доступа";
+console.log(content);
+
+
+// let myAge = 32;
+// console.log("my age =", myAge);
