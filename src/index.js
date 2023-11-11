9da621e58cc852f9db4b49ac6d8635530a3cd3fb
Yevhenii Melnyk <yevheniimelnyk7@gmail.com>
2023-11-11 12:34:05 +0200

    reinstaling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-// alert('Hello world!');
+jQuery(document).ready(function ($) {
 
-// let age = prompt("promt");
-// console.log(age);
-
-let yourAge = confirm("Вам есть 18 лет?");
-var content = yourAge ? "18+" : "Нет доступа";
-console.log(content);
-
-
-// let myAge = 32;
-// console.log("my age =", myAge);
+});
