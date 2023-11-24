96576aff68505db06b5ecaea2ce2a595612fb024
Yevhenii Melnyk <yevheniimelnyk7@gmail.com>
2023-11-24 16:41:13 +0200

    24.11.2023

---
+++ home.js
@@ -0,0 +1,32 @@
+// const age = prompt("Сколько вам лет?");
+
+
+// function checkAge(usersAge){
+//     console.log("userAge", usersAge)
+//     if (!usersAge){
+//         return console.log("Вы не ввели возраст!")
+//     }
+//     if(usersAge >= 18){
+//        return console.log("Проходите!") }
+//         else{
+//         console.log("Доступ запрещен!")
+//     }
+// }
+
+// checkAge(age);
+
+//----------------------------------------------------------
+
+// const ready = confirm ("Вы готовы войти?");
+
+
+// if(ready){
+//         const newName = prompt("Введите ваше имя")
+//         console.log(`Ваше имя: ${newName}`)
+//     } 
+
+// else{
+//     console.log("Вы не вошли")
+// }
+
+//----------------------------------------------------------
